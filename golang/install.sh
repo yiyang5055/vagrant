c9568743fa3213be2cc7a0fae716a54ef8219b2d
yiyang5055 <10083282+yiyang5055@users.noreply.github.com>
2019-09-27 15:35:04 +0800

    Update install.sh

--- golang/install.sh
+++ golang/install.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 cd ~/golang && rm -rf src/golang.org/ && git clone https://github.com/golang/tools.git src/golang.org/x/tools
+cd golang.org/x/lint/golint && git checkout release-branch.go1.9
 go install golang.org/x/tools/cmd/goimports
 go install golang.org/x/tools/cmd/guru
 go install golang.org/x/tools/cmd/gorename
-go install golang.org/x/lint/golint
-go get -v github.com/stamblerre/gocode
+
+## install plugin
+go get -v github.com/nsf/gocode
 go get -v github.com/golangci/golangci-lint/cmd/golangci-lint
 go get -v github.com/rogpeppe/godef
-go get -v github.com/kisielk/errcheck
 go get -v github.com/jstemmer/gotags
 go get -v github.com/klauspost/asmfmt/cmd/asmfmt
 go get -v github.com/fatih/motion
@@ -15,3 +16,11 @@ go get -v github.com/fatih/gomodifytags
 go get -v github.com/zmb3/gogetdoc
 go get -v github.com/josharian/impl
 go get -v github.com/kardianos/govendor
+
+## install errcheck
+git checkout master
+go get -v github.com/kisielk/errcheck
+
+## install golint
+cd ~/golang/src/golang.org/x/ && git clone https://github.com/golang/lint.git lint/golint
+go install golang.org/x/lint/golint
